57c1941706c8db096987f63d17e61084007a3680
Igor Kislyuk <glyerkg@gmail.com>
2016-07-18 23:50:00 +0300

    Initial Commit

---
+++ UnwindSeguesCustomAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnwindSeguesCustomAnimation
+//
+//  Created by Igor on 18/07/16.
+//  Copyright © 2016 Igor Kislyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
